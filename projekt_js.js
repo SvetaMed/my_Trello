0e89caabc268a903759091b9dd5a5cffb7d164c1
Svitlana Medvedieva <inf.na12@gmail.com>
2022-02-01 10:57:06 +0200

    add user on the header

--- projekt_js.js
+++ projekt_js.js
@@ -160,3 +160,21 @@ dragNdrop()
 // 	}
 // }
 // delBoard()
+
+async function getUser(){
+    let user=await fetch('https://randomuser.me/api/')
+    let json=await user.json()
+    console.log(json)
+    let newUser = {
+        gender: json.results[0].gender,
+        name: json.results[0].name,
+        pic:json.results[0].picture.thumbnail
+    }
+    let users = document.querySelector('.users')
+    users.insertAdjacentHTML('beforeend',`
+        <div class="user">
+            <img src="${newUser.pic}" alt="">
+        </div>
+    `)
+    console.log(newUser)
+}
